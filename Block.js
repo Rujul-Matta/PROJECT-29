b2dbc29cf475c761ae550f7648af3d85449a2684
Rujul-Matta <74641349+Rujul-Matta@users.noreply.github.com>
2020-11-21 17:32:09 +0530

    Project completed

---
+++ Block.js
@@ -0,0 +1,13 @@
+class Block{
+    constructor(x, y){
+        this.body = Bodies.rectangle(x,y,25,45,{isStatic: false}); 
+        this.width = 25;
+        this.height = 45;
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+    }
+}
